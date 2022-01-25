994bf35656a926603291fbc00f451d3f864d6bcb
Eric Hackman <kwesihackman@gmail.com>
2022-01-25 12:28:44 +0000

    feat: implemented deleting a product

--- src/components/MainApp/ProductListItem.tsx
+++ src/components/MainApp/ProductListItem.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { getCurrentPrice } from '../../utils/Functions';
+import * as action from '../../redux/actions/actions';
 import { IProduct, IProductPrices } from '../../utils/Types';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import { useDispatch } from 'react-redux';
 
 type Props = {
   product: IProduct;
@@ -14,12 +18,19 @@ export type Inputs = {
 };
 
 const ProductListItem = ({ product, index, prices }: Props) => {
+  const dispatch = useDispatch();
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
   const currentProductPrices = prices.find(
     (priceList) => priceList.productId === product.id
   );
   const currentProductPrice = getCurrentPrice(
     currentProductPrices!.prices
   ).price;
+
+  const handleDeleteProduct = () => {
+    console.log(product);
+    dispatch(action.DeleteProduct(index));
+  };
 
   return (
     <React.Fragment>
@@ -29,8 +40,35 @@ const ProductListItem = ({ product, index, prices }: Props) => {
         <td>{currentProductPrice.toFixed(2)}</td>
         <td className="cursor-pointer">View</td>
         <td className="cursor-pointer">Edit</td>
-        <td className="text-danger cursor-pointer">Delete</td>
+        <td
+          role="button"
+          className="text-danger cursor-pointer"
+          onClick={() => setShowDeleteConfirmation(true)}
+        >
+          Delete
+        </td>
       </tr>
+
+      <Modal
+        show={showDeleteConfirmation}
+        onHide={() => setShowDeleteConfirmation(false)}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>{`Delete ${product.name}`}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{`Are you sure you want to delete  ${product.name}?`}</Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="secondary"
+            onClick={() => setShowDeleteConfirmation(false)}
+          >
+            Close
+          </Button>
+          <Button variant="danger" onClick={handleDeleteProduct}>
+            Confirm Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </React.Fragment>
   );
 };
