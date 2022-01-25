78a77bf567d740ebf00d55b4101d50d2549e3080
Eric Hackman <kwesihackman@gmail.com>
2022-01-25 11:04:19 +0000

    fix: updated the app logos and favicon in public folder, updated the manifest file for the project

---
+++ src/utils/Constants.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = 'mPharma Product Management';
+export const STORAGE_KEY = 'mpharma_state_value';
+export const FETCH_PRODUCT_ENDPOINT = '/5c3e15e63500006e003e9795';
+export const ERROR_SAVING_STATE =
+  'Oops! something went wrong try to save products. Please try again';
